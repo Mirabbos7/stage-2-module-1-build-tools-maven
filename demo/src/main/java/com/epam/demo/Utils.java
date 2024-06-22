658527e07927a8d2f7a3121b2199be0684e6cfeb
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-06-22 16:46:13 +0500

    Test commit

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,7 +1,5 @@
 package com.epam.demo;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.ArrayList;
 import java.util.List;
 
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,6 +1,9 @@
 package com.epam.utils;
 
 public class StringUtils {
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("-123456"));
+    }
     public static boolean isPositiveNumber(String str) {
         if (str == null || str.isEmpty()) {
             return false;
